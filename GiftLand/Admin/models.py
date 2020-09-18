4be237df412dd57fbd4730bf59d5eefc2fe03304
ummejui <ummejui161999@gmail.com>
2020-09-18 14:11:02 +0600

    fixed admin

--- GiftLand/Admin/models.py
+++ GiftLand/Admin/models.py
@@ -5,7 +5,7 @@ class Admin(models.Model):
     admin_id = models.IntegerField(blank=True, null=True)
     admin_name = models.CharField(max_length=100)
     admin_email = models.CharField(max_length=200, unique=True)
-    admin_phone = models.IntegerField(blank=True, null=True)
+    admin_phone = models.IntegerField(max_length=11, blank=True, null=True)
 
     product = models.ManyToManyField(Product)
 

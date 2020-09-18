497fc8a6ca9c4bc0bd984a8cbd7270ed01904811
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2020-09-18 11:52:37 +0600

    fixed templates

--- GiftLand/GiftLand/settings.py
+++ GiftLand/GiftLand/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'GiftLand.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

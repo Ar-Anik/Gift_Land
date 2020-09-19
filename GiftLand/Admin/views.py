2b33f87ed2245ec4cabf3a4d225b158f509fac47
FahimAnayet <fahimanayet@gmail.com>
2020-09-19 09:39:43 +0600

    Garbage Deleted

--- GiftLand/Admin/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Admin
-# Create your views here.
-def Show_Admin(request):
-    admin_info = Admin.objects.all()
-    context={
-        'admin_info':admin_info,
-    }
-    return render(request,'admin.html',context) 

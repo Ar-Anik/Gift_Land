58b6e1d45a635eee77265e60bcb940963881f333
FahimAnayet <fahimanayet@gmail.com>
2020-09-19 16:45:31 +0600

    Fix issues Delivery

--- GiftLand/user/models.py
+++ GiftLand/user/models.py
@@ -2,7 +2,8 @@ from django.db import models
 #from Product .models import*
 # Create your models here.
 
-class user(models.Model) :
+
+class user(models.Model):
     u_id = models.BigIntegerField(blank=True)
     u_name = models.CharField(max_length=100, blank=True)
     u_email = models.EmailField(max_length=50, blank=True)
@@ -13,22 +14,26 @@ class user(models.Model) :
     def __str__(self):
         return str(self.id)
 
-class order(models.Model) :
+
+class order(models.Model):
     o_id = models.BigIntegerField(blank=True)
-    o_status = models.CharField(max_length=200,blank=True)
+    o_status = models.CharField(max_length=200, blank=True)
     o_date = models.DateTimeField(auto_now_add=True)
 
-    user = models.ForeignKey(user, on_delete=models.CASCADE, default=1, null=True)
+    user = models.ForeignKey(
+        user, on_delete=models.CASCADE, default=1, null=True)
 
-    def __str__(self) :
+    def __str__(self):
         return str(self.o_id)
+
 
 class payment(models.Model):
     payment_id = models.BigIntegerField(blank=True)
     payment_type = models.CharField(max_length=200, blank=True)
     amount = models.DecimalField(max_digits=20, decimal_places=2)
 
-    user = models.ForeignKey(user, on_delete=models.SET_NULL, default=1, null=True)
+    user = models.ForeignKey(
+        user, on_delete=models.SET_NULL, default=1, null=True)
 
     def __str__(self):
         return str(self.payment_id)
@@ -37,7 +42,8 @@ class payment(models.Model):
 class delivery(models.Model):
     delivery_code = models.BigIntegerField(blank=True)
 
-    #order = models.ForeignKey(order, on_delete=models.CASCADE, default=1, null=True)
+    order = models.ForeignKey(
+        order, on_delete=models.CASCADE, default=1, null=True)
 
     def __str__(self):
         return str(self.delivery_code)
